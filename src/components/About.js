0b7dbe9417747f2b69345cc784229212c738956b
KaiSar <116952116+Kaisar003@users.noreply.github.com>
2023-06-07 11:08:28 +0000

    Update 9 files

---
+++ src/components/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <p>Hello, wishes from rideontrek.com</p>
+      <br />
+      <p>
+        Trek Bicycle Corporation is a bicycle and cycling product manufacturer
+        and distributor under brand names Trek, Electra Bicycle Company,
+        Bontrager, and Diamant Bikes. The company has previously manufactured
+        bikes under the Gary Fisher, LeMond Racing Cycles, Klein, and Villiger
+        Bikes brand names.
+      </p>
+      <br />
+      <p>
+        Trek bikes are very good. Trek has a long history of making quality
+        bikes that are used by beginner, hobby, and elite cyclists around the
+        world. Trek bikes last for a long time and have excellent reselling
+        value years after purchase.
+      </p>
+      <br />
+    </div>
+  );
+}
+
+export default About;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
+import React from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
 
 function Cards() {
   return (
-    <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
+    <div className="cards">
+      <h1>Check out these Upcoming Events!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
+              src="https://cdn.pixabay.com/photo/2015/03/15/14/53/kids-674513_1280.jpg"
+              text="Join our volunteer event in India and make a positive impact by contributing your time and skills towards a worthy cause"
+              label="India"
+              path="/services"
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
+              src="https://cdn.pixabay.com/photo/2017/02/07/04/12/african-2044961_1280.jpg"
+              text="Are you passionate about making a positive impact on children's lives? Join our volunteer event in Africa, where we aim to create a joyful and memorable experience for children in need"
+              label="Africa"
+              path="/services"
             />
           </ul>
-          <ul className='cards__items'>
+          <ul className="cards__items">
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
+              src="https://cdn.pixabay.com/photo/2017/05/31/18/38/sea-2361247_1280.jpg"
+              text="We are organizing a volunteer event focused on ocean conservation. This event aims to raise awareness about the importance of preserving our oceans and protecting marine life."
+              label="Peru"
+              path="/services"
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
+              src="images/img-4.jpg"
+              text="Experience Football on Top of the Himilayan Mountains"
+              label="Adventure"
+              path="/products"
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
+              src="images/img-8.jpg"
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="Adrenaline"
+              path="/sign-up"
             />
           </ul>
         </div>
