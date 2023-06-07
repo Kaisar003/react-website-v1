5bbda623fbf4c3c28f647ef2cdecb5cff405eadc
KaiSar <116952116+Kaisar003@users.noreply.github.com>
2023-06-07 14:24:34 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+import Cards from "./components/Cards";
+import About from "./components/About";
 
 function App() {
   return (
@@ -13,10 +15,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path="/" exact component={Home} />
+          <Route path="/events" component={Services} />
+          <Route path="/Cards" component={Cards} />
+          <Route path="/About" component={About} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,52 @@
 import React from "react";
 import "../../App.css";
+import "./Cards.css";
+import CardItem from "./CardItem";
 
-export default function Services() {
-  return <h1 className="events">Events</h1>;
+function Cards() {
+  return (
+    <div className="events">
+      <h1>Check out these Upcoming Events!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="https://cdn.pixabay.com/photo/2015/03/15/14/53/kids-674513_1280.jpg"
+              text="Join our volunteer event in India and make a positive impact by contributing your time and skills towards a worthy cause"
+              label="India"
+              path="/events"
+            />
+            <CardItem
+              src="https://cdn.pixabay.com/photo/2017/02/07/04/12/african-2044961_1280.jpg"
+              text="Are you passionate about making a positive impact on children's lives? Join our volunteer event in Africa, where we aim to create a joyful and memorable experience for children in need"
+              label="Africa"
+              path="/events"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="https://cdn.pixabay.com/photo/2017/05/31/18/38/sea-2361247_1280.jpg"
+              text="We are organizing a volunteer event focused on ocean conservation. This event aims to raise awareness about the importance of preserving our oceans and protecting marine life."
+              label="Peru"
+              path="/events"
+            />
+            <CardItem
+              src="https://images.pexels.com/photos/12314915/pexels-photo-12314915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              text="Get your hands dirty and make a difference in our community! We're organizing a community garden planting event at Gardenville"
+              label="Gardenville"
+              path="/products"
+            />
+            <CardItem
+              src="https://cdn.pixabay.com/photo/2018/10/13/20/04/garbage-3745004_1280.jpg"
+              text="Join us for a beach cleanup event at Sunny Shores Beach. Help us protect the marine environment by picking up litter, sorting recyclables, and spreading awareness about the importance of keeping our beaches clean"
+              label="Coastal City"
+              path="/sign-up"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Cards;
